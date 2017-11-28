2fb4e7178f9bcbb5bd7795d510d3f13fe9df355c
JLMacarit <jl.macarit@ymail.com>
2017-11-28 18:18:23 +0100

    debut algo echelonnement

--- cs.js
+++ cs.js
@@ -3,11 +3,13 @@ var ordre=document.getElementById("ordre");
 var ok=document.getElementById("okbutton");
 var matrice=document.getElementById("matrice");
 var string="";
+var ech=document.getElementById("echelon");
+var possible = false;
 
 //génère les cases de la matrice 
 
 ok.onclick=function(){
-    if (ordre.value!=null){
+    if (ordre.value!=null && ordre.value<21){
         string="";
 
         for (i=0;i<ordre.value;i++){
@@ -21,7 +23,37 @@ ok.onclick=function(){
 
         }
         matrice.innerHTML=string;
-
+        possible=true;
     }
     
+}
+
+var tableauTemp; // deja declaree et passee avant
+var k=0,l=0;
+// tableau
+var profil, prochainIndiceProfil=0;
+// recherche de l'ordre des lignes pour echelonnement
+ech.onclick=function(){    
+    var col = k;
+    while(possible && col <= ordre.value){        
+        
+        var lig = l;
+        // tant que pas trouve le pivot DANS LA COLONNE 
+        var notfound = true;
+        while (notfound && lig <= ordre.value) {
+            if(tableauTemp[l][k] != 0){
+                profil[prochainIndiceProfil]=lig;
+                ++prochainIndiceProfil;
+                notfound=false;
+
+
+
+
+
+
+            }
+            ++lig;
+        }
+        ++col;
+    }
 }
