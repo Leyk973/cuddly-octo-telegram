ee1f142b7e2dff9989b5df940e2eeecbd2d49979
Unknown <thophilecandelier@yahoo.fr>
2017-11-29 18:05:13 +0100

    Affichage plus propre et nouvelle fonctionnalité de génération aléatoire

--- cs.js
+++ cs.js
@@ -9,10 +9,12 @@ var n=0;
 var m;
 var profildemescouilles=[];
 var lignetemporaire=[];
+var random=document.getElementById("random");
 //génère les cases de la matrice 
 
 ok.onclick=function(){
     if (ordre.value!=null && ordre.value<21){
+        document.getElementById("random").disabled=false;
         m=new Array(ordre.value);
         for (i=0;i<ordre.value;i++){
             m[i]=new Array(ordre.value);
@@ -23,7 +25,7 @@ ok.onclick=function(){
             string+="<tr>";
             for (j=0;j<ordre.value;j++){
                 string+="<td>";
-                string+="<input type='number' id='"+i+","+j+"' value=0 />";
+                string+="<input type='number' id='"+i+","+j+"' value='0' class=mat onclick=this.select(); />";
                 string+="</td>";
                 
             }
@@ -43,38 +45,51 @@ echelon.onclick=function(){
     for (i=0;i<n;i++){
         for (j=0;j<n;j++){
             m[i][j]=document.getElementById(i+","+j).value;
-
+            
         }
     }
 
 
-    console.log(m);
+    
     
     while(possible && k <ordre.value){
-        //RECHERCHE
-        var lig=l;
-        var notfound=true;
-        while (notfound && lig<ordre.value){
-            if (m[lig][k]!=0){
-                profildemescouilles.push(lig);
-                notfound=!notfound;
-            }
-            lig++;
-        }
+        //recherche();
+       
         
+        
+    }
 
-        if (!nontrouve){
-            if (profildemescouilles[0]!=l){
-                for (i=0;i<ordre.value;i++){
-                    lignetemporaire[i]=m[profildemescouilles[0]][i];
-                    m[profildemescouilles[0]][i]=m[l][i];
-                    m[l][i]=lignetemporaire[i];
-                }
+    console.log(m);
+}
+
+
+function recherche(){
+    var lig=l;
+    var notfound=true;
+    while (notfound && lig<ordre.value){
+        if (m[lig][k]!=0){
+            profildemescouilles.push(lig);
+            notfound=false;
+        }
+        lig++;
+    }
+    
+
+    if (!notfound){
+        if (profildemescouilles[0]!=l){
+            for (i=0;i<ordre.value;i++){
+                lignetemporaire[i]=m[profildemescouilles[0]][i];
+                m[profildemescouilles[0]][i]=m[l][i];
+                m[l][i]=lignetemporaire[i];
             }
         }
-        
-        
     }
 }
 
 
+random.onclick=function(){
+    var inputs=document.getElementsByClassName("mat");
+    for (i=0;i<inputs.length;i++){
+        inputs[i].value=Math.round((Math.random()-0.5)*20);
+    }
+}
