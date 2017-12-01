090074c3c0521a8feef5f69d45f70a8de67e3eec
Unknown <thophilecandelier@yahoo.fr>
2017-12-01 16:06:08 +0100

    Echelonnement fonctionnel
    L'algo de l'échelonnement a été complété et corrigé, on peut échelonner plusieurs fois sans avoir besoin de recharger la page, il reste cependant un petit problème d'arrondis pour les zéros.
    Il faudra également vérifier les résultats manuellement

--- cs.js
+++ cs.js
@@ -10,6 +10,7 @@ var m;
 var profildemescouilles=[];
 var lignetemporaire=[];
 var random=document.getElementById("random");
+var inputs=document.getElementsByClassName("mat");
 //génère les cases de la matrice 
 
 ok.onclick=function(){
@@ -34,6 +35,15 @@ ok.onclick=function(){
         }
         matrice.innerHTML=string;
         possible=true;
+
+        //Quand on laisse une case vide, la valeur 0 lui est attribuée
+        for (i=0;i<inputs.length;i++){
+            inputs[i].onblur=function(){
+                if (this.value==""){
+                    this.value=0;
+                }
+            }
+        }
     }
     
 }
@@ -42,54 +52,75 @@ ok.onclick=function(){
 var l=0,k=0;
 
 echelon.onclick=function(){
+    l=0;
+    k=0;
     for (i=0;i<n;i++){
         for (j=0;j<n;j++){
             m[i][j]=document.getElementById(i+","+j).value;
-            
         }
     }
-
+    
 
     
     
-    while(possible && k <ordre.value){
-        //recherche();
+    while(l<ordre.value && k <ordre.value){
+        profildemescouilles=[];
+        var lig=l;
+        var notfound=true;
+        while (notfound && lig<ordre.value){
+            if (m[lig][k]!=0){
+                profildemescouilles.push(lig);
+                notfound=false;
+            }
+            lig++;
+        }
+        
+    
+        if (!notfound){
+            if (profildemescouilles[0]!=l){
+                for (i=0;i<ordre.value;i++){
+                    lignetemporaire[i]=m[profildemescouilles[0]][i];
+                    m[profildemescouilles[0]][i]=m[l][i];
+                    m[l][i]=lignetemporaire[i];
+                }
+            }
+                
+                
+            for (i=l+1;i<n;i++){
+                var facteur=m[i][k]/m[l][k];
+                console.log(facteur);
+                for (j=k;j<n;j++){
+                    m[i][j]-=facteur*m[l][j];
+                }
+            }
+
+            l++;
+            
+           
+        }
+        k++;
+
+        
        
         
         
     }
-
+    for (i=0;i<m.length;i++){
+        for (j=0;j<m.length;j++){
+            document.getElementById(i+","+j).value=m[i][j];
+        }
+    }   
     console.log(m);
-}
 
-
-function recherche(){
-    var lig=l;
-    var notfound=true;
-    while (notfound && lig<ordre.value){
-        if (m[lig][k]!=0){
-            profildemescouilles.push(lig);
-            notfound=false;
-        }
-        lig++;
-    }
     
-
-    if (!notfound){
-        if (profildemescouilles[0]!=l){
-            for (i=0;i<ordre.value;i++){
-                lignetemporaire[i]=m[profildemescouilles[0]][i];
-                m[profildemescouilles[0]][i]=m[l][i];
-                m[l][i]=lignetemporaire[i];
-            }
-        }
-    }
 }
+
 
 
 random.onclick=function(){
-    var inputs=document.getElementsByClassName("mat");
     for (i=0;i<inputs.length;i++){
         inputs[i].value=Math.round((Math.random()-0.5)*20);
     }
 }
+
+
