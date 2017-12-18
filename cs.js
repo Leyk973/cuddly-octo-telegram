0aa03c6e1180ee6f54952d15f110e9d0c7ec3635
JLMacarit <jl.macarit@ymail.com>
2017-12-19 00:40:25 +0100

    debug deter

--- cs.js
+++ cs.js
@@ -227,86 +227,46 @@ echelon.onclick=function(){
         break;
         
         case "cholesky" :
-        document.getElementById("matl").style.position="relative";
-        document.getElementById("matu").style.position="relative";
-        document.getElementById("matl").style.visibility="visible";
-        document.getElementById("matu").style.visibility="visible";
-
-        document.getElementById("egal2").style.position="relative";
-        document.getElementById("egal2").style.visibility="visible";
-        document.getElementById("ixe2").style.visibility="visible";
-        document.getElementById("ixe2").style.position="relative";
         var mat, ci, cj, ck, somme;
         // récupération de la matrice initiale
-        mat=new Array(n);
         for (ci=0;ci<n;ci++){
-            mat[ci]=new Array(n);
             for (cj=0;cj<n;cj++){
                 mat[ci][cj]=document.getElementById(ci+","+cj).value;
             }
         }
-        if (isSymetric(mat)){
-            matlchol = new Array(n);
-            for (ci=0;ci<n;ci++){
-                matlchol[ci] = new Array(n);
-            }
-    
-            for (ci=0; ci<n; ci++){
-                for(cj=0; cj<n; cj++){
-                    matlchol[ci][cj]=0;
-                }
-            }
-            //algo d'André-Louis
-            matlchol[0][0] = Math.sqrt(mat[0][0]);
-            for (cj=1; cj<n; cj++){
-                matlchol[cj][0] = mat[cj][0] / matlchol[0][0];
-            }
-            for (ci=1; ci<n-1; ci++){
-                somme = 0;
-                for (ck=0; ck<ci-1; ck++){
-                    somme += Math.pow(matlchol[ci][ck],2);
-                }
-                matlchol[ci][ci] = Math.sqrt(mat[ci][ci] - somme);
-                for (cj=ci+1; cj<n; cj++){
-                    somme=0;
-                    for (ck=0; ck<(ci-1); ck++){
-                        somme+=(matlchol[cj][ck] * matlchol[ci][ck]);
-                    }
-                    matlchol[cj][ci]= (mat[cj][ci] - somme) / matlchol[ci][ci];
-                }
-            }
-            matlchol[n-1][n-1] = Math.sqrt(mat[n-1][n-1] - somme);
-
-        
-
-        
-            str="";
-            for (i=0;i<n;i++){
-                str+="<tr>";
-                for (j=0;j<n;j++){
-                    str+="<td><label>"+String(matlchol[i][j]).substring(0,6)+"</label></td>";
-                    
-                }
-                str+="</tr>";
-            }
-            document.getElementById("matl").innerHTML=str;
-
-            str="";
-            for (i=0;i<n;i++){
-                str+="<tr>";
-                for (j=0;j<n;j++){
-                    str+="<td><label>"+String(matlchol[j][i]).substring(0,6)+"</label></td>";
-                    
-                }
-                str+="</tr>";
-            }
-            document.getElementById("matu").innerHTML=str;
-    
-            
-        }else{
-            window.alert("La matrice n'est pas symétrique et définie positive.");
+        matlchol = new Array(n);
+        for (ci=0;ci<n;ci++){
+            matlchol[ci] = new Array(n);
         }
-        
+
+        for (ci=0; ci<n; ci++){
+            for(cj=0; cj<n; cj++){
+                matlchol[ci][cj]=0;
+            }
+        }
+
+        //algo d'André-Louis
+        matlchol[0][0] = Math.sqrt(mat[0][0]);
+        for (cj=1; cj<n; cj++){
+            matlchol[cj][0] = mat[cj][0] / matlchol[0][0];
+        }
+        for (ci=1; ci<n-1; ci++){
+            somme = 0;
+            for (ck=0; ck<ci-1; ck++){
+                somme += Math.pow(matlchol[ci][ck],2);
+            }
+            matlchol[ci][ci] = Math.sqrt(mat[ci][ci] - somme);
+            for (cj=ci+1; cj<n; cj++){
+                somme=0;
+                for (ck=0; ck<(ci-1); ck++){
+                    somme+=(matlchol[cj][ck] * matlchol[ci][ck]);
+                }
+                matlchol[cj][ci]= (mat[cj][ci] - somme) / matlchol[ci][ci];
+            }
+        }
+        matlchol[n-1][n-1] = Math.sqrt(mat[n-1][n-1] - somme);
+
+
         break;
 
         
@@ -343,24 +303,13 @@ echelon.onclick=function(){
 
 
 random.onclick=function(){
-    if (methode!="cholesky"){
-        for (i=0;i<inputs.length;i++){
-            inputs[i].value=Math.round((Math.random()-0.5)*20);
-        }
-        var inputsb=document.getElementsByClassName("matb");
-        for (i=0;i<inputsb.length;i++){
-            inputsb[i].value=Math.round((Math.random()-0.5)*20);
-        }
-    }else{
-        randomSymetric(n);
-        for (var i=0;i<n;i++){
-            for (var j=0;j<n;j++){
-                document.getElementById(i+","+j).value=m[i][j];
-            }
-            document.getElementById("b"+i).value=b[i];
-        }
+    for (i=0;i<inputs.length;i++){
+        inputs[i].value=Math.round((Math.random()-0.5)*20);
     }
-    
+    var inputsb=document.getElementsByClassName("matb");
+    for (i=0;i<inputsb.length;i++){
+        inputsb[i].value=Math.round((Math.random()-0.5)*20);
+    }
 }
 
 
@@ -395,16 +344,6 @@ resoudre.onclick=function(){
         
         break;
         case "cholesky":
-        solutionCholesky();
-        if (pasdesolution){
-            window.alert("Pas de solution.");
-        }else{
-            var s="";
-            for (i=0;i<x.length;i++){
-                s+="<tr><td>x"+(i+1)+" = </td><td>"+x[i]+"</td></tr>"
-            }
-            document.getElementById("x").innerHTML=s;
-        }
         break;
     }
 
@@ -420,29 +359,6 @@ function solutionIterative(){
         }
         if (m[i][i]!=0){
             x[i]/=m[i][i];
-        }else{
-            pasdesolution=true;
-        }
-        
-    }
-}
-
-function solutionCholesky(){
-    pasdesolution=false;
-    for (var i=0;i<n;i++){
-        y[i]=b[i];
-        for (var j=0;j<i;j++){
-            y[i]-=matlchol[i][j]*y[j];
-        }
-    }
-
-    for (var i=n-1;i>=0;i--){
-        x[i]=y[i];
-        for (var j=i+1;j<n;j++){
-            x[i]-=matlchol[j][i]*x[j];
-        }
-        if (matlchol[i][i]!=0){
-            x[i]/=matlchol[i][i];
         }else{
             pasdesolution=true;
         }
@@ -561,10 +477,6 @@ function calcDetByLU(matP,nP){
             matu[i][j]=matP[i][j];
         }
     }
-    //var u = matu;
-
-    //var u = matP;
-
     // Construction de L et U
     for (k = 0; k < nP; ++k){
         p = matu[k][k];
@@ -579,8 +491,10 @@ function calcDetByLU(matP,nP){
 
     // Calcul du déterminant
     for (i = 0 ; i < nP; ++i){
+        console.log("boucle : "+i+"  "+matu[i][i]);
         det *= matu[i][i];
     }
+    console.log("debug fin cdblu : "+detrmnt);
 
     i= iniI;
     j= iniJ;
@@ -598,91 +512,13 @@ determiner.onclick=function(){
         }
     }
     detrmnt = calcDetByLU(matini,n);
+    console.log(detrmnt);
+    detrmnt = Math.round(detrmnt*10000)/10000;
     window.alert("determinant de A : " + detrmnt);
     if ((detrmnt != 0) && (detrmnt != null)){
         inverser.disabled=false;
     }
 }
-
-
-
-
-// NOPE ON VA FAIRE CA PAR GAUSS JORDAN
-/*
-//ne pas oublier la puissance de -1
-inverser.onclick=function(){
-    if ((detrmnt == 0)  || (isNaN(detrmnt))){
-        console.log("determinant de A nul : matrice non inversible");
-    } else if (detrmnt == null){
-        console.log("veuillez d'abord calculer le determinant");
-    } else {
-        console.log("calcul de l'inverse");
-        // initialisation de la matrice adjointe
-        var matAdj;
-        matAdj=new Array(n);
-        for (i=0;i<n;i++){
-            matAdj[i]=new Array(n)
-        }
-        // calcul des coefficients de adj
-        var sousMat;
-        sousMat=new Array(n-1);
-        for (i=0;i<n-1;i++){
-            sousMat[i]=new Array(n-1)
-        }
-
-//debug afficahge matini
-for (di=0;di<n;++di){
-    for (dj=0;dj<n;++dj){
-        console.log("matini("+di+","+dj+")="+matini[di][dj]);
-    }
-}
-
-        var cofi, cofj;
-
-        for (i=0;i<n;i++){
-            console.log("dansI---"+i+" n vaut ---" + n);
-            for (j=0;j<n;++j){
-                console.log("dansJ---"+i+"/"+j+"---");
-                //sous matrice
-                cofi=0;
-                for (inti=0;inti<n;++inti){
-                    if (inti != i){ // ligne a prendre en compte pour le calcul du cofacteur
-                        cofj=0;
-                        for (intj=0;intj<n;++intj){
-                            if(intj != j){ // colonne a prendre en compte pour le calcul du cofacteur
-                                sousMat[cofi][cofj]=matini[inti][intj];
-                                ++cofj;
-                            }
-                        }
-                        ++cofi;
-                    }                    
-                }
-                
-                console.log("---"+i+"/"+j+"---");
-                //debug afficahge sousmat
-                for (di=0;di<n-1;++di){
-                    for (dj=0;dj<n-1;++dj){
-                        console.log("sousmat["+i+j+"]("+di+","+dj+")="+sousMat[di][dj]);
-                    }
-                }
-
-
-                // deter sous mat
-                matAdj[i][j] = Math.pow(-1,i+j);
-                matAdj[i][j] *= calcDetByLU(sousMat,n-1);
-                console.log("matAdj("+i+","+j+") = "+matAdj[i][j]);
-            }
-            console.log("FIN I ---"+i+" n vaut ---" + n);
-            console.log("FIN I2 ---"+i+" n vaut ---" + n);
-        }
-
-        // passage a la transposée
-        // matInv <- tr(adj)
-        // matInv *= 1/det
-    }
-
-}
-*/
 
 inverser.onclick = function () {
     
@@ -715,6 +551,9 @@ inverser.onclick = function () {
                         m[l][i] = lignetemporaire[i];
                     }
                 }
+
+                // normalisation
+
 
 
                 for (i = l + 1; i < n; i++) {
@@ -811,6 +650,7 @@ document.getElementById("comparaison").onclick=function(){
             }
             b[i]=Math.round(Math.random()*3);
         }
+        console.log(r);
         for (var i=0;i<nb;i++){
             for (var j=0;j<nb;j++){
                 m[i][j]=0;
